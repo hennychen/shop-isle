f7a6695e9bfcaef0a386d8e93d66f1c5c887d643
rodica-andronache <rodik_andronache@yahoo.com>
2015-07-02 17:38:27 +0300

    Finished slider, added banners and some part of the latest products

--- README.md
+++ README.md
@@ -2,6 +2,13 @@
 
 Images sources:
 
-https://download.unsplash.com/uploads/141172692104151d94dd4/4f900e54
-https://download.unsplash.com/photo-1434222986057-dbade8843377
-https://download.unsplash.com/reserve/RONyPwknRQOO3ag4xf3R_Kinsey.jpg
+ * slide1.jpg: https://download.unsplash.com/uploads/141172692104151d94dd4/4f900e54
+ * slide2.jpg: https://download.unsplash.com/photo-1434056886845-dac89ffe9b56
+ * slide3.jpg: https://download.unsplash.com/reserve/RONyPwknRQOO3ag4xf3R_Kinsey.jpg
+
+
+ * banner1.jpg: https://download.unsplash.com/photo-1433826672293-6fdc46138e66
+ * banner2.jpg: https://download.unsplash.com/photo-1433643667043-663b34a5c052
+ * banner3.jpg: https://download.unsplash.com/photo-1435070872030-a8113da23691
+
+License: All unsplash.com images are licensed under the terms of the Creative Commons Zero, http://creativecommons.org/publicdomain/zero/1.0/ 	
--- front-page.php
+++ front-page.php
@@ -3,50 +3,42 @@
 	<!-- Home start -->
 	<section id="home" class="home-section home-parallax home-fade home-full-height">
 
-		<div class="hero-slider">
-			
-				<?php
+	<?php
 					
-					$shop_isle_slider_section = get_theme_mod('shop_isle_slider',json_encode(array( array("image_url" => get_template_directory_uri().'/assets/images/slide1.jpg' ,"link" => "#" ),array("image_url" => get_template_directory_uri().'/assets/images/slide2.jpg' ,"link" => "#" ),array("image_url" => get_template_directory_uri().'/assets/images/slide3.jpg' ,"link" => "#" ) )));
+		$shop_isle_slider = get_theme_mod('shop_isle_slider',json_encode(array( array("image_url" => get_template_directory_uri().'/assets/images/slide1.jpg' ,"link" => "#", "text" => "ShopIsle", "subtext" => "WooCommerce Theme", "label" => "FIND OUT MORE" ),array("image_url" => get_template_directory_uri().'/assets/images/slide2.jpg' ,"link" => "#", "text" => "ShopIsle", "subtext" => "Hight quality store" , "label" => "FIND OUT MORE"),array("image_url" => get_template_directory_uri().'/assets/images/slide3.jpg' ,"link" => "#", "text" => "ShopIsle", "subtext" => "Responsive Theme" , "label" => "FIND OUT MORE") )));
 					
-					if( !empty( $shop_isle_slider_section ) ){
+		if( !empty( $shop_isle_slider ) ){
 						
+			$shop_isle_slider_decoded = json_decode($shop_isle_slider);
 						
-						$parallax_one_social_icons_decoded = json_decode($shop_isle_slider_section);
-						
-						if( !empty($parallax_one_social_icons_decoded) ){
-						
-							echo '<ul class="slides">';
+			if( !empty($shop_isle_slider_decoded) ){
 							
-								foreach($parallax_one_social_icons_decoded as $parallax_one_social_icon){
+				echo '<div class="hero-slider">';
+						
+					echo '<ul class="slides">';
+							
+						foreach($shop_isle_slider_decoded as $parallax_one_social_icon){
 									
-									echo '<li class="bg-dark-30 bg-dark" style="background-image:url('.$parallax_one_social_icon->image_url.')">
-										<div class="hs-caption">
-											<div class="caption-content">
-												<div class="hs-title-size-1 font-alt mb-30">
-													This is Rival
-												</div>
-												<div class="hs-title-size-4 font-alt mb-30">
-													Summer 2015
-												</div>
-												<div class="hs-title-size-1 font-alt mb-40">
-													Your online fashion destination
-												</div>
-												<a href="#latest" class="section-scroll btn btn-border-w btn-round">Learn More</a>
-											</div>
-										</div>
-									</li>';
+							echo '<li class="bg-dark-30 bg-dark" style="background-image:url('.$parallax_one_social_icon->image_url.')">';
+								echo '<div class="hs-caption">';
+									echo '<div class="caption-content">';
+										echo '<div class="hs-title-size-4 font-alt mb-30">'.$parallax_one_social_icon->text.'</div>';
+										echo '<div class="hs-title-size-1 font-alt mb-40">'.$parallax_one_social_icon->subtext.'</div>';
+										echo '<a href="'.$parallax_one_social_icon->link.'" class="section-scroll btn btn-border-w btn-round">'.$parallax_one_social_icon->label.'</a>';
+									echo '</div>';
+								echo '</div>';
+							echo '</li>';
 								
-								}
-						
-							echo '</ul>';
-						
 						}
-					}
+						
+					echo '</ul>';
+							
+				echo '</div>';
+						
+			}
+		}
 					
-				?>
-
-		</div>
+	?>
 
 	</section >
 	<!-- Home end -->
@@ -55,22 +47,39 @@
 	<div class="main">
 
 		<!-- Banner bloks start -->
-		<?php if ( is_active_sidebar( 'sidebar-banners' ) ) : ?>
 		
-			<section class="module-small">
-				<div class="container">
+		<?php
+					
+			$shop_isle_banner = get_theme_mod('shop_isle_banner',json_encode(array( array("image_url" => get_template_directory_uri().'/assets/images/banner1.jpg' ,"link" => "#" ),array("image_url" => get_template_directory_uri().'/assets/images/banner2.jpg' ,"link" => "#"),array("image_url" => get_template_directory_uri().'/assets/images/banner3.jpg' ,"link" => "#") )));
+					
+			if( !empty( $shop_isle_banner ) ){
+						
+				$shop_isle_banner_decoded = json_decode($shop_isle_banner);
+						
+				if( !empty($shop_isle_banner_decoded) ){
+							
+					echo '<section class="module-small">';
+						echo '<div class="container">';
 
-					<div class="row shop_isle_banners_section">
+							echo '<div class="row shop_isle_banners_section">';
+							
+								foreach($shop_isle_banner_decoded as $parallax_one_social_icon){
+									
+									echo '<div class="col-sm-4"><div class="content-box mt-0 mb-0"><div class="content-box-image"><a href="'.$parallax_one_social_icon->link.'"><img src="'.$parallax_one_social_icon->image_url.'"></a></div></div></div>';
+								
+								}
+						
+							echo '</div>';
+							
+						echo '</div>';
+							
+					echo '</section>';
+						
+				}
+			}
+					
+	?>
 
-						<?php dynamic_sidebar('sidebar-banners'); ?>
-
-					</div><!-- .row -->
-
-				</div><!-- .container -->
-			</section>
-			
-		<?php endif; ?>
-		
 		<!-- Banner bloks end -->
 
 		<!-- Divider -->
@@ -87,135 +96,52 @@
 					</div>
 				</div><!-- .row -->
 
-				<div class="row multi-columns-row">
+				<?php
 				
-					<?php echo do_shortcode('[recent_products per_page="12" columns="4"]'); ?>
+				$shop_isle_latest_args = array( 'post_type' => 'product', 'posts_per_page' => 8, 'orderby' =>'date','order' => 'DESC' );
+				
+				$shop_isle_latest_loop = new WP_Query( $shop_isle_latest_args );
+				
+				if( $shop_isle_latest_loop->have_posts() ):
+				
+					echo '<div class="row multi-columns-row">';
+				
+					while( $shop_isle_latest_loop->have_posts() ) : 
+					
+						$shop_isle_latest_loop->the_post(); 
+						global $product; 
+						
+						echo '<div class="col-sm-6 col-md-3 col-lg-3">';
+							echo '<div class="shop-item">';
+								echo '<div class="shop-item-image">';
+								
+									if (has_post_thumbnail( $shop_isle_latest_loop->post->ID )):
+										echo get_the_post_thumbnail($shop_isle_latest_loop->post->ID, 'shop_catalog'); 
+									else: 
+										echo '<img src="'.woocommerce_placeholder_img_src().'" alt="Placeholder" width="65px" height="115px" />';
+									endif;
+									
+									echo '<div class="shop-item-detail">';
+										echo '<a class="btn btn-round btn-b" href="<?php echo $product->add_to_cart_url(); ?>"><span class="icon-basket"></span> Add To Cart</a>';
+									echo '</div>';
+								echo '</div>';
+								echo '<h4 class="shop-item-title font-alt"><a href="#"><?php the_title(); ?></a></h4>';
+								echo get_woocommerce_currency_symbol().$product->price;
+							
+							echo '</div>';
+						echo '</div>';
 
-					<!-- Shop item start -->
-					<div class="col-sm-6 col-md-3 col-lg-3">
-						<div class="shop-item">
-							<div class="shop-item-image">
-								<img src="<?php echo get_template_directory_uri(); ?>/assets/images/shop/product-7.jpg" alt="">
-								<div class="shop-item-detail">
-									<a class="btn btn-round btn-b"><span class="icon-basket"></span> Add To Cart</a>
-								</div>
-							</div>
-							<h4 class="shop-item-title font-alt"><a href="#">Accessories Pack</a></h4>
-							L9.00
-						</div>
-					</div>
-					<!-- Shop item end -->
-
-					<!-- Shop item start -->
-					<div class="col-sm-6 col-md-3 col-lg-3">
-						<div class="shop-item">
-							<div class="shop-item-image">
-								<img src="<?php echo get_template_directory_uri(); ?>/assets/images/shop/product-8.jpg" alt="">
-								<div class="shop-item-detail">
-									<a class="btn btn-round btn-b"><span class="icon-basket"></span> Add To Cart</a>
-								</div>
-							</div>
-							<h4 class="shop-item-title font-alt"><a href="#">Men’s Casual Pack</a></h4>
-							L12.00
-						</div>
-					</div>
-					<!-- Shop item end -->
-
-					<!-- Shop item start -->
-					<div class="col-sm-6 col-md-3 col-lg-3">
-						<div class="shop-item">
-							<div class="shop-item-image">
-								<img src="<?php echo get_template_directory_uri(); ?>/assets/images/shop/product-9.jpg" alt="">
-								<div class="shop-item-detail">
-									<a class="btn btn-round btn-b"><span class="icon-basket"></span> Add To Cart</a>
-								</div>
-							</div>
-							<h4 class="shop-item-title font-alt"><a href="#">Men’s Garb</a></h4>
-							L6.00
-						</div>
-					</div>
-					<!-- Shop item end -->
-
-					<!-- Shop item start -->
-					<div class="col-sm-6 col-md-3 col-lg-3">
-						<div class="shop-item">
-							<div class="shop-item-image">
-								<img src="<?php echo get_template_directory_uri(); ?>/assets/images/shop/product-10.jpg" alt="">
-								<div class="shop-item-detail">
-									<a class="btn btn-round btn-b"><span class="icon-basket"></span> Add To Cart</a>
-								</div>
-							</div>
-							<h4 class="shop-item-title font-alt"><a href="#">Cold Garb</a></h4>
-							L14.00
-						</div>
-					</div>
-					<!-- Shop item end -->
-
-					<!-- Shop item start -->
-					<div class="col-sm-6 col-md-3 col-lg-3">
-						<div class="shop-item">
-							<div class="shop-item-image">
-								<img src="<?php echo get_template_directory_uri(); ?>/assets/images/shop/product-11.jpg" alt="">
-								<div class="shop-item-detail">
-									<a class="btn btn-round btn-b"><span class="icon-basket"></span> Add To Cart</a>
-								</div>
-							</div>
-							<h4 class="shop-item-title font-alt"><a href="#">Accessories Pack</a></h4>
-							L9.00
-						</div>
-					</div>
-					<!-- Shop item end -->
-
-					<!-- Shop item start -->
-					<div class="col-sm-6 col-md-3 col-lg-3">
-						<div class="shop-item">
-							<div class="shop-item-image">
-								<img src="<?php echo get_template_directory_uri(); ?>/assets/images/shop/product-12.jpg" alt="">
-								<div class="shop-item-detail">
-									<a class="btn btn-round btn-b"><span class="icon-basket"></span> Add To Cart</a>
-								</div>
-							</div>
-							<h4 class="shop-item-title font-alt"><a href="#">Men’s Casual Pack</a></h4>
-							L12.00
-						</div>
-					</div>
-					<!-- Shop item end -->
-
-					<!-- Shop item start -->
-					<div class="col-sm-6 col-md-3 col-lg-3">
-						<div class="shop-item">
-							<div class="shop-item-image">
-								<img src="<?php echo get_template_directory_uri(); ?>/assets/images/shop/product-13.jpg" alt="">
-								<div class="shop-item-detail">
-									<a class="btn btn-round btn-b"><span class="icon-basket"></span> Add To Cart</a>
-								</div>
-							</div>
-							<h4 class="shop-item-title font-alt"><a href="#">Men’s Garb</a></h4>
-							L6.00
-						</div>
-					</div>
-					<!-- Shop item end -->
-
-					<!-- Shop item start -->
-					<div class="col-sm-6 col-md-3 col-lg-3">
-						<div class="shop-item">
-							<div class="shop-item-image">
-								<img src="<?php echo get_template_directory_uri(); ?>/assets/images/shop/product-14.jpg" alt="">
-								<div class="shop-item-detail">
-									<a class="btn btn-round btn-b"><span class="icon-basket"></span> Add To Cart</a>
-								</div>
-							</div>
-							<h4 class="shop-item-title font-alt"><a href="#">Cold Garb</a></h4>
-							L14.00
-						</div>
-					</div>
-					<!-- Shop item end -->
-
-				</div><!-- .row -->
+					endwhile; 
+					
+					echo '</div>';
+					
+				endif;
+				
+				wp_reset_postdata(); ?>
 
 				<div class="row mt-30">
 					<div class="col-sm-12 align-center">
-						<a href="shop-grid-col-3.html" class="btn btn-b btn-round">See all products</a>
+						<a href="<?php echo get_permalink( woocommerce_get_page_id( 'shop' )); ?>" class="btn btn-b btn-round">See all products</a>
 					</div>
 				</div>
 
@@ -290,9 +216,4 @@
 				
 			endif;	
 			?>
-		
-
-		<!-- Divider -->
-		<hr class="divider-w">
-		<!-- Divider -->
 <?php get_footer(); ?>
