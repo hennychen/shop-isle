f6e87da2462076b8280bd9c2bee5acd9656c1d80
claudiuvertistudio <claudiu.buruiana@codeinwp.com>
2016-02-19 11:26:51 +0200

    Fixed #134 Display search and cart in header only when woocommerce is activated

--- inc/structure/header.php
+++ inc/structure/header.php
@@ -84,30 +84,31 @@ if ( ! function_exists( 'shop_isle_primary_navigation' ) ) {
 						</div>
 					</div>
 
-					<div class="navbar-cart">
-						
-						<div class="header-search">
-							<div class="glyphicon glyphicon-search header-search-button"></div>
-							<div class="header-search-input">
-								<form role="search" method="get" class="woocommerce-product-search" action="<?php echo esc_url( home_url( '/'  ) ); ?>">
-									<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search Products&hellip;', 'placeholder', 'woocommerce' ); ?>" value="<?php echo get_search_query(); ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label', 'woocommerce' ); ?>" />
-									<input type="submit" value="<?php echo esc_attr_x( 'Search', 'submit button', 'woocommerce' ); ?>" />
-									<input type="hidden" name="post_type" value="product" />
-								</form>
+					<?php if( class_exists( 'WooCommerce' ) ): ?>
+						<div class="navbar-cart">
+							
+							<div class="header-search">
+								<div class="glyphicon glyphicon-search header-search-button"></div>
+								<div class="header-search-input">
+									<form role="search" method="get" class="woocommerce-product-search" action="<?php echo esc_url( home_url( '/'  ) ); ?>">
+										<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search Products&hellip;', 'placeholder', 'woocommerce' ); ?>" value="<?php echo get_search_query(); ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label', 'woocommerce' ); ?>" />
+										<input type="submit" value="<?php echo esc_attr_x( 'Search', 'submit button', 'woocommerce' ); ?>" />
+										<input type="hidden" name="post_type" value="product" />
+									</form>
+								</div>
 							</div>
+
+							<?php if( function_exists( 'WC' ) ): ?>
+								<div class="navbar-cart-inner">
+									<a href="<?php echo WC()->cart->get_cart_url() ?>" title="<?php _e( 'View your shopping cart','shop-isle' ); ?>">
+										<span class="icon-basket"></span>
+										<span class="cart-item-number"><?php echo trim( WC()->cart->get_cart_contents_count() ); ?></span>
+									</a>
+								</div>
+							<?php endif; ?>
+
 						</div>
-
-						<?php if( function_exists( 'WC' ) ): ?>
-							<div class="navbar-cart-inner">
-								<a href="<?php echo WC()->cart->get_cart_url() ?>" title="<?php _e( 'View your shopping cart','shop-isle' ); ?>">
-									<span class="icon-basket"></span>
-									<span class="cart-item-number"><?php echo trim( WC()->cart->get_cart_contents_count() ); ?></span>
-								</a>
-							</div>
-						<?php endif; ?>
-
-					</div>
-
+					<?php endif; ?>
 	
 				</div>
 			</div>
