8ea7a951683c42ac2f8b3cf1c66ecf9fa49d1a1d
claudiuvertistudio <claudiu.buruiana@codeinwp.com>
2016-06-22 12:37:05 +0300

    !!! #119 Shop page layout issue when more products

--- functions.php
+++ functions.php
@@ -12,5 +12,3 @@ require get_template_directory() . '/inc/init.php';
  * Note: Do not add any custom code here. Please use a child theme so that your customizations aren't lost during updates.
  * http://codex.wordpress.org/Child_Themes
  */
-
-add_filter( 'loop_shop_per_page', create_function( '$cols', 'return 100;' ), 20 );
